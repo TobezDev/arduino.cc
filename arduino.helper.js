c684f217820893063b8ad87c50bc50d98a35ed6d
TheRealTobes28-Codez <83095177+TheRealTobes28-Codez@users.noreply.github.com>
2023-01-19 16:10:42 +0000

    remove helper (.js)

--- arduino.helper.js
+++
@@ -1,6 +0,0 @@
-const arduino = require('arduino.js');
-const script = require('./arduino.cpp');
-
-const ctrl_panel = document.getElementsByClassName('html');
-
-ctrl_panel.content = ctrl_panel.val();
